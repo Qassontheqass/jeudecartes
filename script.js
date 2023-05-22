7b65b88706597c8cc9d5a914c4ae9da6357a2878
Steve <steve@gmail.com>
2023-05-22 09:08:31 +0200

    buttonmodified

---
+++ script.js
@@ -0,0 +1,4 @@
+let btnAttackGoku = document.querySelector(#attackGoku)
+let btnHealGoku = document.querySelector(#healGoku)
+let btnAttackVegeta = document.querySelector(#attackVegeta)
+let btnHealVegeta = document.querySelector(#healVegeta)
